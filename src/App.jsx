d093d8df40cecff9de8b201ab489241dec94dfdc
Paulo Vieira <paulohvieirajr@hotmail.com>
2024-08-26 20:48:39 -0300

    Estrutura do modulo 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import styled from "styled-components"
+import EstilosGlobais from './componentes/EstilosGlobais'
 
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  /* Pelo Menos 100% da tela: View Port - vh*/
+  min-height: 100vh; 
+`
+
+function App() {
   return (
-    <>
-      
-    </>
+    <FundoGradiente>
+      <EstilosGlobais />
+    </FundoGradiente>
   )
 }
 
